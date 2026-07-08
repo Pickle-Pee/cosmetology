82e6ce3497640fcb68782a1f1be3cfa6e50327fa
Kirill Magurin <55835923+Pickle-Pee@users.noreply.github.com>
2026-07-08 15:03:57 +0300

    Add about and training routes

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,6 +1,8 @@
 import Layout from "./layout/Layout.jsx";
 import Home from "../pages/Home.jsx";
+import About from "../pages/About.jsx";
 import Services from "../pages/Services.jsx";
+import Training from "../pages/Training.jsx";
 import Prices from "../pages/Prices.jsx";
 import Articles from "../pages/Articles.jsx";
 import Reviews from "../pages/Reviews.jsx";
@@ -13,13 +15,15 @@ export const routes = [
     element: <Layout />,
     children: [
       { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
       { path: "/services", element: <Services /> },
+      { path: "/training", element: <Training /> },
       { path: "/prices", element: <Prices /> },
       { path: "/articles", element: <Articles /> },
       { path: "/reviews", element: <Reviews /> },
       { path: "/contacts", element: <Contacts /> },
-      { path: "*", element: <NotFound /> },
       { path: "/articles/:slug", element: <ArticlePage /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ];
